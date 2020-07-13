65bae89534e03c2ba59733276d96072b95371e2a
System Administrator <root@Fanny-Fox.lan>
2020-07-13 18:41:24 +0100

    first commit

---
+++ client/js/app/views/MensagemView.js
@@ -0,0 +1,21 @@
+class MensagemView extends View{
+
+/*	constructor(elemento){
+
+		this._elemento = elemento;
+	}*/
+	 constructor(elemento) {
+        super(elemento);
+    }
+
+	template(model) {
+
+    	return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '<p></p>';
+  	}
+	/*update(model) {
+
+		this._elemento.innerHTML = this.template(model);
+	}*/
+
+
+}
---
+++ client/js/app/views/NegociacoesView.js
@@ -0,0 +1,89 @@
+class NegociacoesView extends View{
+
+    /*constructor(elemento) {
+
+        this._elemento = elemento;
+    }*/
+     constructor(elemento) {
+        super(elemento);
+    }
+
+
+    template(model){
+
+    return `
+    <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                        <th>VOLUME</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    ${model.negociacoes.map(n => `
+
+                          <tr>
+                            <td>${DateHelper.dataParaTexto(n.data)}</td>
+                            <td>${n.quantidade}</td>
+                            <td>${n.valor}</td>
+                            <td>${n.volume}</td>
+                          </tr>
+
+                      `).join('')}
+                </tbody>
+                <tfoot>
+                    <td colspan="3"></td>
+                    <td>
+                        ${ model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}
+                    </td>
+                </tfoot>
+
+
+         </table>
+        `;
+
+      }
+
+
+    //update(model){
+
+    //  this._elemento.innerHTML = this._template(model);
+   // }
+
+/* Esse exemplo é para quando ouver mais de um retorno 
+    _template(model){
+
+    return `
+    <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                        <th>VOLUME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${model.negociacoes.map(n => {
+
+                      return `
+                        <tr>
+                            <td>${DateHelper.dataParaTexto(n.data)}</td>
+                            <td>${n.quantidade}</td>
+                            <td>${n.valor}</td>
+                            <td>${n.volume}</td>
+                        </tr>
+                      `
+                      }).join('')}
+                </tbody>
+         </table>
+        `;
+      }
+*/
+
+
+
+}
